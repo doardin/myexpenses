e5694395ab7573a9d822ccc5fd86fb9453e8baea
Eduardo Lopes <duh.lopes234@hotmail.com>
2023-07-02 14:15:18 -0300

    feat: Initial commit

---
+++ src/test/java/br/com/doardin/myexpenses/MyexpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doardin.myexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyexpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
