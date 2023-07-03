ea743c4bd35779eddff7944e92f282aef2667ea5
Eduardo Lopes <duh.lopes234@hotmail.com>
2023-07-02 22:28:06 -0300

    feat: add notblank validation at field name

--- src/main/java/br/com/doardin/myexpenses/application/category/dto/PostCategoryDto.java
+++ src/main/java/br/com/doardin/myexpenses/application/category/dto/PostCategoryDto.java
@@ -2,8 +2,9 @@ package br.com.doardin.myexpenses.application.category.dto;
 
 import br.com.doardin.myexpenses.annotations.validenum.ValidEnum;
 import br.com.doardin.myexpenses.enums.CategoryTypes;
+import jakarta.validation.constraints.NotBlank;
 
 public record PostCategoryDto(
-        String name,
+        @NotBlank String name,
         @ValidEnum(enumClass = CategoryTypes.class) String type) {
 }
